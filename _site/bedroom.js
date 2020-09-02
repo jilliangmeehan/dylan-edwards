51f204b239e6cd1799610842d6981f44aad4fc02
Jillian Meehan <jillian@lickability.net>
2020-09-02 16:25:37 -0400

    categories header changes to current category
    progress on #28

--- _site/bedroom.js
+++ _site/bedroom.js
@@ -37,11 +37,13 @@ function removeClass(element, name) {
 
 var btnContainer = document.getElementById("categories-btns");
 var btns = btnContainer.getElementsByClassName("btn");
+var label = document.getElementById("category-label");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function(){
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
+    label.innerHTML = this.innerHTML;
   });
 }
 
